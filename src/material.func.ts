5dbec2a7ac5f82b30897956bd99902ba3ae58cbc
it1shka <2tishbel@gmail.com>
2023-10-20 00:14:40 +0200

    finished material function

--- src/material.func.ts
+++ src/material.func.ts
@@ -10,15 +10,34 @@ type History = Array<{
   readonly amount: number
 }>
 
-const material = (spaceship: number[]): History => {
+const material = (spaceship: number[]) => {
   if (spaceship.length === 0) {
     return []
   }
-  // let leftPointer = 0
-  // let rightPointer = spaceship.length - 1
-  // let leftMax = spaceship[leftPointer]
-  // let rightMax = spaceship[]
-  return []
+  let leftPointer = 0
+  let rightPointer = spaceship.length - 1
+  let leftMax = spaceship[leftPointer]
+  let rightMax = spaceship[rightPointer]
+
+  const history: History = []
+
+  while (leftPointer < rightPointer) {
+    let index: number, amount: number
+    if (leftMax < rightMax) {
+      amount = leftMax - spaceship[leftPointer]
+      index = leftPointer
+      leftPointer++
+      leftMax = Math.max(leftMax, spaceship[leftPointer])
+    } else {
+      amount = rightMax - spaceship[rightPointer]
+      index = rightPointer
+      rightPointer--
+      rightMax = Math.max(rightMax, spaceship[rightPointer])
+    }
+    history.push({index, amount})
+  }
+
+  return history
 }
 
 export default material
